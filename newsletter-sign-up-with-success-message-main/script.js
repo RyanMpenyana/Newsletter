9f05da2c698b9102247c5a3f107205d8916e10d0
admin <admin@MacHDs-MacBook-Pro.local>
2023-07-14 15:22:03 +0200

    js update

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -2,41 +2,42 @@ const emailBtn = document.getElementById('btn');
 const dismissBtn = document.getElementById('dismiss-btn');
 const emailInput = document.getElementById('email');
 const invalidMail = document.getElementById('invalid-email');
+const overwrite = document.getElementById('email-variation')
 
 
-
-function getEmailValue(inputField){
-    emailInput.textContent = inputField;
-
+function parseEnteredEmail(parse) {
+    overwrite.textContent = parse;
 }
-
-function errorHandle(){
+function errorHandle() {
     emailInput.style.backgroundColor = 'hsla(0, 80%, 64% , 0.5)';
     emailInput.style.border = '1px solid red';
     invalidMail.style.visibility = 'visible';
 }
 
-function errorReset(){
-    emailInput.style = 'default';
-    invalidMail.style = 'default';
+function ErrorReset() {
+    emailInput.style = ""
+    invalidMail.style = ""
 }
-function execution(){
-    if (emailInput.value.includes('com') && emailInput.value.includes('@')  === true) {
+
+function execution() {
+    if (emailInput.value.includes('com') && emailInput.value.includes('@') === true) {
         grantModal();
+        parseEnteredEmail(emailInput.value);
     }
-    else{
-        errorHandle();
+    else {
+        errorHandle()
     }
 }
-function grantModal(){
+
+function grantModal() {
     const Modal = document.getElementById('modal-bg');
     Modal.style.visibility = "visible";
 }
-function hideModal(){
+function hideModal() {
     const hideModal = document.getElementById('modal-bg');
     hideModal.style.visibility = "hidden";
-    emailInput.value = "";
+    emailInput.value = ""
 }
-emailInput.addEventListener('click' , errorReset);
-emailBtn.addEventListener('click' , execution);
+emailInput.addEventListener('click', ErrorReset);
+emailBtn.addEventListener('click', execution);
 dismissBtn.addEventListener('click', hideModal);
